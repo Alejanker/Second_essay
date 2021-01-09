7048b214e5ff841c9d423c8660584b29e3273665
Alejandro Cuello <nanodevep@gmail.com>
2021-01-09 15:29:40 -0500

    cambio minimo

--- parse_mol.py
+++ parse_mol.py
@@ -1,67 +1,35 @@
 
 def mol_counter(formula, i):
     
-    i = i
-    last = ''
-    mol = {}
     number = ''
-  
+    mol = {}
+
     while i < len(formula):
-        # print(formula[i])
-        if formula[i] in ('(', '[', '}'):
-          
-            second, i = mol_counter(formula, i+1)
-            for idx in second:
-                if mol.get(idx) != None:
-                    mol[idx] += second[idx]
-                    print(mol)
-                else:
-                    mol[idx] = second[idx]
-                    print('mol idx', mol[idx], idx)
-            print('i', i, 'len', len(formula))
-
-        elif formula[i] in (')','}',']'):
-            try:
-                if formula[i+1].isdigit():
-                    number = int(formula[i+1])
-                    for idx in mol:
-                        mol[idx] *= number
-                    return mol , i 
-                else:
-                    return mol , i 
-            except IndexError:
-                return mol , i
-                
-        elif formula[i].isdigit():
-            if name != "":
-                name += formula[i]
+        if formula[i].isdigit():
+            if number == '':
+                number = formula[i]
             else:
-                name = formula[i]
-
-            mol[last] = int(name)
-            i += 1
-            continue
-
-        elif not formula[i].islower() and mol.get(formula[i]) == None:
-            mol[formula[i]] = 1
-            last = formula[i]
+                number += formula[i]
+            print(number)
+            mol[ formula[i-1] ] = int(number)
+        
+        elif formula[i].isupper() and mol.get(formula[i]) == None:
+            mol[ formula[i] ] = 1
+        
         else:
-            if mol.get(last):
-                del mol[last]
-            
-            mol[last + formula[i]] = 1
-            last = last + formula[i]
+            if i-1 > -1 and i -1 < len(formula):
+                del mol[ formula[i-1]]
+
+                mol[formula[i-1] + formula[i] ] = 1
 
         i += 1
-        name = ""
 
-    return mol , i
-
+    return mol
 
 def parse_molecule (formula):
     # detectar cuandXo estan encerradas en parentesis
     # multiplicar 
-    mol, _ = mol_counter(formula, 0)
+    mol = mol_counter(formula, 0)
 
     return mol
         
