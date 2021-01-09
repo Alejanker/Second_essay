8ac913295f9d7353ba2d3b18d21f889ef350a3a8
Alejandro Cuello <nanodevep@gmail.com>
2021-01-09 17:22:43 -0500

    primer intento

--- parse_mol.py
+++ parse_mol.py
@@ -1,30 +1,29 @@
 
 def mol_counter(formula, i):
-    
-    number = ''
+    last_integer = None
     mol = {}
 
     while i < len(formula):
-        if formula[i].isdigit():
-            if number == '':
-                number = formula[i]
+        it = formula[i]
+
+        if it.isdigit():
+            if flast_integer == None:
+                last_integer = it
             else:
-                number += formula[i]
-            print(number)
-            mol[ formula[i-1] ] = int(number)
-        
-        elif formula[i].isupper() and mol.get(formula[i]) == None:
-            mol[ formula[i] ] = 1
-        
-        else:
-            if i-1 > -1 and i -1 < len(formula):
-                del mol[ formula[i-1]]
-
-                mol[formula[i-1] + formula[i] ] = 1
-
+                last_integer += it
+            mol[it] = int(last_integer)
+            
+        elif it.isupper() and mol.get(it) == None:
+            mol[it] = 1
+        elif it.islower():
+            if i - 1 > -1:
+                del mol[formula[i-1]]
+                mol[formula[i - 1] + it] = 1
+            
         i += 1
+             
+            
 
-    return mol
 
 def parse_molecule (formula):
     # detectar cuandXo estan encerradas en parentesis
