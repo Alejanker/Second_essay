aaef90f8184240aa405c7bf4de618a2f41e4647d
Alejandro Cuello <nanodevep@gmail.com>
2021-01-10 16:09:34 -0500

    algoritmo funcional

--- parse_mol.py
+++ parse_mol.py
@@ -1,43 +1,89 @@
+def parse_mol(formula, i=0):
+    number = None
 
-def mol_counter(formula, i):
-    last_integer = None
+    name = ''
     mol = {}
+
+    temporal = 0
 
     while i < len(formula):
         it = formula[i]
 
-        if it.isdigit():
-            if flast_integer == None:
-                last_integer = it
+        if it in (')', ']', '}'):
+            if i + 1 < len(formula):
+                for idx in formula[i+1:]:
+                    if idx.isdigit():
+                        if number == None:
+                            number = idx
+                        else:
+                            number += idx
+                        i += 1
+                    else:
+                        break
+                
+            if number == None:
+                return mol, i 
             else:
-                last_integer += it
-            mol[it] = int(last_integer)
+                number = int(number)
+
+                for idx in mol:
+                    mol[idx] *= number
+                
+                return mol, i
+
+        elif it in ('(' , '[', '{'):
+            second , i = parse_mol(formula, i+1)
+
+            for idx in second:
+                if idx in mol:
+                    mol[idx] += second[idx]
+                else:
+                    mol[idx] = second[idx]
+
+        elif it.isdigit():
+            number = ''
+            for idx in formula[i:]:
+                if idx.isdigit():
+                    number += idx
+                    i += 1
+                else:
+                    break
             
-        elif it.isupper() and mol.get(it) == None:
-            mol[it] = 1
-        elif it.islower():
-            if i - 1 > -1:
-                del mol[formula[i-1]]
-                mol[formula[i - 1] + it] = 1
+            number = int(number)
+            valor = number + (mol[name] if name in mol else 0)
             
+            if temporal == 1:
+                valor -= 1
+
+            mol[name] = valor
+
+            number = None
+            temporal = 0
+            continue
+            
+        else:
+            valor = 1
+            for idx in formula[i:]:
+                if idx.isupper() and valor:
+                    name = idx
+                    valor -= 1
+                elif idx.islower():
+                    name += idx
+                    i += 1
+                else:
+                    break
+
+            mol[name] = (mol[name] if name in mol else 0) + 1
+            temporal = 1  
+    
         i += 1
-             
-            
 
-
-def parse_molecule (formula):
-    # detectar cuandXo estan encerradas en parentesis
-    # multiplicar 
-    mol = mol_counter(formula, 0)
-
-    return mol
-        
-        
-        
+    return mol, i
 
 
 if __name__ == '__main__':
-    formula = input('Ingrese formula: ')
-    a = parse_molecule(formula)
+    formula = input('Mol: ')
 
-    print('formula: ' + str(a))
+    a, _ = parse_mol(formula)
+
+    print(a)
